ae7bee789a32cf55c247c14683002d2ce5c284d1
André Bauer <a.bauer@ovm-kassel.de>
2018-12-08 22:08:58 +0100

    Add preserveNumbers to ISBN10.java.

--- src/main/java/ISBN10.java
+++ src/main/java/ISBN10.java
@@ -1,4 +1,9 @@
 public class ISBN10 {
+
+  public static String preserveNumbers(String s) {
+    return s.replaceAll("[^0-9]","");
+  }
+
   public static char checkDigit(String isbn) {
     if (isbn.equals("1-55860-491")) {
       return 'X';
