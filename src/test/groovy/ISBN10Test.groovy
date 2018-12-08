1ccde5291b9081e7704980694a8822f033c69d00
André Bauer <a.bauer@ovm-kassel.de>
2018-12-08 20:59:20 +0100

    Refactor checksum test: expect char as result type.

--- src/test/groovy/ISBN10Test.groovy
+++ src/test/groovy/ISBN10Test.groovy
@@ -4,12 +4,12 @@ class ISBN10Test extends Specification {
 
     def "the ISBN10 3-8171-2004-4 has the checksum 4"() {
         expect:
-        ISBN10.checksum("3-8171-2004-4") == 4
+        ISBN10.checksum("3-8171-2004-4") == '4'
     }
 
     def "the ISBN10 1-55860-491 has the checksum X"() {
         expect:
-        ISBN10.checksum("1-55860-491") == "X"
+        ISBN10.checksum("1-55860-491") == 'X'
     }
 }
 
