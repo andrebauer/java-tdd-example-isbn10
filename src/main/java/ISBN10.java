173e272debb1e490fe5db4111b89f9ff43241668
André Bauer <a.bauer@ovm-kassel.de>
2018-12-11 13:08:02 +0100

    Add javadoc comments.

--- src/main/java/ISBN10.java
+++ src/main/java/ISBN10.java
@@ -1,12 +1,34 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+/**
+ * Functions that compute the ISBN10 check digit based on the
+ * first nine figures, given in a string.
+ *
+ * @author André Bauer
+ * @version 1.0
+ */
 public class ISBN10 {
-
+    
+  /**
+   * Filters out all characters of a given string, except figures.
+   * 
+   * @param s the input string.
+   * @return A string, that only contains the figures from the input string.
+   */
   public static String preserveNumbers(String s) {
     return s.replaceAll("[^0-9]","");
   }
 
+  /**
+   * Transforms a string to an array of figures. It ignores characters that 
+   * are not figures. Each single character that is a figure will be
+   * convertet to an int and gets
+   * his own entry in the resulting array.
+   *
+   * @param s the input string.
+   * @return An int array, that contains the figures of the input string.
+   */
   public static int[] toIntArray(String s) {
     return preserveNumbers(s)
       .chars()
@@ -15,6 +37,14 @@ public class ISBN10 {
       .toArray();
   }
 
+  /**
+   * Computes the ISBN10 check digit. The input is an arbitrary string.
+   * Only if the sring contains exact nine figures, a valid ISBN10 check
+   * will be computed. In all other cases the output is unspecified.
+   *
+   * @param isbn a string, that should contain exact nine figures.
+   * @return The ISBN10 check digit-
+   */
   public static char checkDigit(String isbn) {
     int[] ints = toIntArray(isbn);
     int sum = IntStream.range(0, ints.length)
